2ef009e48c36dbab35128c54a6fd835feaaba98b
Student <dimadianov23@gmail.com>
2026-04-23 06:50:40 +0300

    gugugaga

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("student/<int:student_id>/", views.student_detail, name="student_detail"),
+    path("/project/<int:project_id>/like/", views.like_project, name="like_project"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Student, Project
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Student, Project, Like
 
 
 def index(request):
@@ -10,3 +10,13 @@ def index(request):
 def student_detail(request, student_id):
     student = get_object_or_404(Student, id=student_id)
     return render(request, "main/student_detail.html", {"student": student})
+
+def like_project(request, project_id):
+    ip = request.META.get('REMOTE_ADDR')
+    project = get_object_or_404(Project, id=project_id)
+    if Like.objects.filter(project=project, ip_address=ip).exists():
+        Like.objects.create(ip_address=ip)
+        Like.save()
+        return redirect('project_detail', project_id=project.id)
+
+    return render(request, "main/project_detail.html", {"gugugaga": Like.objects.values('ip_address').count()})
