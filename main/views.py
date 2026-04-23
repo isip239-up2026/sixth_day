d5aa9b9bb30b4aa28fb00ee43455f8ce790528a4
DetectiveRedacted <imlovemonolith@gmsil.com>
2026-04-23 11:50:47 +0900

    Task2 ну вроде бы работает @DetectiveRedacted

--- main/views.py
+++ main/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Student, Project
 
-
 def index(request):
     students = Student.objects.all()
     return render(request, "main/index.html", {"students": students})
 
-
 def student_detail(request, student_id):
     student = get_object_or_404(Student, id=student_id)
-    return render(request, "main/student_detail.html", {"student": student})
+    projects = student.projects.all()
+    return render(request, "main/student_detail.html", {
+        "student": student,
+        "projects": projects
+    })
+
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    return render(request, "main/project_detail.html", {"project": project})
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'main',
 ]
 
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
