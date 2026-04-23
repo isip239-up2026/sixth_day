3977d772fcfec4f84194129281e62398c2bea820
VXRWO <batochuk@gmail.com>
2026-04-23 12:38:06 +0900

    task 1.2

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from seed import skills
+from seed import skills 
 from .models import Student, Project, StudentSkill
 
 
@@ -12,6 +12,8 @@ def index(request):
 def student_detail(request, student_id):
     student = get_object_or_404(Student, id=student_id)
     skills=StudentSkill.objects.filter(student=student).select_related('skill')
-    return render(request, "main/student_detail.html", {"student": student},{"skills": skills})
+    return render(request, "main/student_detail.html",
+                  {"student": student,
+                  "skills": skills})
 
 
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,6 +80,7 @@ DATABASES = {
     }
 }
 
+DEFAULT_CHARSET = 'utf-8'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
